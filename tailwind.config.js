7ec11cea015e0125c5a37c8321fb43d8d9c2d21d
Enzo C <enzo20112002@gmail.com>
2023-05-29 17:19:09 +0200

    ajout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.html', './js/**/*.js'],
+  content: ['./pages/**/*.html', './js/**/*.js', './index.html'],
   theme: {
     extend: {},
+
+    backgroundColor: theme => ({
+       ...theme('colors'),
+       'bleuFonce': '#0d2840',
+       'bleuClair': '#d6e8f4',
+       'danger': '#e3342f',
+      })
   },
   plugins: [],
 }
